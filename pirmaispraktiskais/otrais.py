ea2181314e08bb315c3b28660629a11b0fbb2c79
flackx <93722831+flackx@users.noreply.github.com>
2022-02-12 12:12:34 +0200

    Add files via upload

---
+++ pirmaispraktiskais/otrais.py
@@ -0,0 +1,12 @@
+def otraisuzd():
+    print("Enter the ammount of hours")
+    x = input()
+    print("Enter hourly rate")
+    y = input()
+    if x > 40:
+        b = x - 40
+        x = 40
+    totalpay = x * y + 17.5 * y
+    print(totalpay)
+    
+otraisuzd()
