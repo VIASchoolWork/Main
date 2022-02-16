954f5c16584f2e6943ee2cac29d6e22f44eb099a
flackx <93722831+flackx@users.noreply.github.com>
2022-02-16 16:05:15 +0200

    Delete pirmaispraktiskais directory

--- pirmaispraktiskais/pirmais.py
+++
@@ -1,9 +0,0 @@
-def firstuzd():
-    print("Enter the ammount of hours")
-    x = input()
-    print("Enter hourly rate")
-    y = input()
-    totalpay = x * y
-    print(totalpay)
-    
-firstuzd()
